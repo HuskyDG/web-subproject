306b3de0461804c8e32cab8a226236dbc574640a
Takeda-senpai <deadboltsx303@gmail.com>
2023-05-08 15:44:24 +0700

    subproject: update more
    Co-Authored-By: tanthien12 <101163035+tanthien12@users.noreply.github.com>

--- subproject/js/loader.js
+++ subproject/js/loader.js
@@ -200,6 +200,27 @@ function getRandomInt2(min, max) {
   return min + Math.floor(Math.random() * (max-min));
 }
 
+
+// my JQuery code
+function load_style() {
+	$(document).ready(function(){
+		$(".img-box").hide();
+		if(window.matchMedia("(max-width: 767px)").matches){
+			$(".grid-container").css("grid-template-areas", "\"a\" \"b\" \"c\" \"d\" \"e\" \"f\" \"g\"");
+		} else if(window.matchMedia("(max-width: 1024px)").matches) {
+			$(".grid-container").css("grid-template-areas", "\"a a\" \"b c\" \"d e\" \"f g\"");
+		} else {
+			$(".img-box").show();
+			$(".grid-container").css("grid-template-areas", "\"a a b c\" \"d e f g\"");
+		}
+	});
+}
+
+$(window).on('resize', function() {
+   load_style();
+});
+
+
 function clockshow(e, imgs, names) {
 	var arrays = [ "a", "b", "c", "d", "e", "f", "g" ];
 	console.log("head");
@@ -214,7 +235,7 @@ function clockshow(e, imgs, names) {
 		text += `
                 <div class="${arrays[i]}">
                     <div class="wimage"><img src="${imgs[i]}" /></div>
-                    <div class="winfo flex">
+                    <div class="winfo flex wrapable">
                         <div class="wname"><b>${names[i]}</b></div>
                         <div class="wprice"><b>Price: ${p}$</b></div>
                     </div>
@@ -224,23 +245,25 @@ function clockshow(e, imgs, names) {
 	console.log("end");
 	text += `</div>`;
 	e.innerHTML += text;
+	load_style();
 }
 
 
-// my JQuery code
-
-$(document).ready(function(){
-    if(window.matchMedia("(max-width: 767px)").matches){
-		$(".img-box").hide();
-		$(".grid-container").css("grid-template-areas", "\"a\" \"b\" \"c\" \"d\" \"e\" \"f\" \"g\"");
-    } else {
-		$(".img-box").show();
-	}
-});
-
 var watchList = [ "Rolex Submariner", "Casio G-Shock", "Timex Weekender", "Fossil Grant", "Tissot V8", "Seiko Prospex", "Bulova Marine Star", "Citizen Eco-Drive", "Omega Seamaster", "Tag Heuer Aquaracer", "Invicta Pro Diver", "Swatch Sistem51", "Hamilton Khaki Field", "Breitling Avenger", "Bell & Ross Aviation", "Longines HydroConquest", "Oris Aquis", "Orient Mako", "Vostok Amphibia", "Luminox Navy Seal", "Fortis B-42", "Sinn U1", "Glycine Combat Sub", "Ball Engineer Master II", "Certina DS Action Diver", "Maurice Lacroix Pontos", "Baume et Mercier Clifton Club", "Rado HyperChrome Captain Cook", "Junghans Max Bill", "Chopard Mille Miglia", "IWC Aquatimer", "Panerai Luminor", "Jaeger-LeCoultre Polaris", "Ulysse Nardin Marine", "Breguet Marine", "Zenith Pilot", "Franck Muller Long Island", "Eterna Kontiki", "Corum Admiral", "Mido Ocean Star", "Tudor Heritage Black Bay", "Raymond Weil Freelancer", "Baume et Mercier Classima", "Hamilton Ventura", "Seiko Presage", "Victorinox Swiss Army Maverick", "Christopher Ward C60 Trident", "Frederique Constant Classics", "Sinn 356", "Alpina Alpiner", "Glycine Airman", "Ball Engineer III", "Rado DiaMaster", "Perrelet First Class", "U-Boat Classico", "Torgoen T16", "Citizen Promaster", "Oris Big Crown", "Longines Master Collection", "Breitling Colt", "TAG Heuer Formula 1", "Bell & Ross Vintage", "Zenith Defy", "Movado Bold", "Skagen Holst", "Bulova Accutron II", "Armani Exchange Hampton", "Diesel Little Daddy", "Guess Maverick", "Michael Kors Bradshaw", "Emporio Armani Renato", "Marc Jacobs Riley", "Nixon Sentry", "Tommy Hilfiger Lexington", "Hugo Boss Jet", "Lacoste 12.12", "Swatch Irony", "Mondaine Classic", "Braun BN0021", "Shinola Runwell", "Rosefield The Bowery", "Olivia Burton Wonderland", "Kate Spade Park Row", "MVMT Classic", "Daniel Wellington Classic Oxford", "Larsson & Jennings Saxon", "Miansai M24", "Timex Expedition Metal Field", "Triwa Nevil", "Shore Projects Project 2", "Corniche Heritage 40", "Void PKG01", "Komono Winston", "Maurice De Mauriac L1", "Uniform Wares M40", "Jaques Lemans Nostalgie", "Paul Smith Baber", "Skagen Hald", "Sevenfriday P3", "Suunto 5" ];
 
-var list = ["./image/shopping_0.webp", "./image/shopping_1.webp", "./image/shopping_2.webp", "./image/shopping_3.webp", "./image/shopping_4.webp", "./image/shopping_5.webp", "./image/shopping_6.webp"]
+var list = ["./image/shopping (1).png",
+"./image/shopping (2).png",
+"./image/shopping (3).png",
+"./image/shopping (4).png",
+"./image/shopping (5).png",
+"./image/shopping (6).png",
+"./image/shopping (7).png",
+ "./image/w1.png",
+ "./image/w2.png",
+ "./image/w3.png",
+ "./image/w4.png",
+ "./image/w5.png",
+ "./image/w6.png" ]
 
 function create_random_list() {
 	var L = [];
